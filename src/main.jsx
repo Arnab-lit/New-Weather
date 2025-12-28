798e1bc3fda428f5da23cbac4f986f49ce7b81fd
Arnab Das <daniyelarnab824@gmail.com>
2025-12-28 23:03:52 +0530

    Aditional

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
+
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
